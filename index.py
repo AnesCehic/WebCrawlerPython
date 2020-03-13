2978e79c57dca0abc3e99182851634d49ed21d7c
Anes Cehic <anes.cehic@gmail.com>
2020-03-13 17:32:35 +0100

    Crawling for links

---
+++ index.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+
+base_url = "https://get.todoist.help"
+url = requests.get(base_url + "/hc/en-us/categories/115000626249")
+
+data = url.text
+soup = BeautifulSoup(data)
+
+faq = soup.find_all("section", attrs={'class': 'dth-section'})
+
+links_and_sections = {}
+
+for links in faq:
+  link = links.h2.a['href']
+  key = links.h2.a.text.strip()
+
+  links_and_sections[key] = []
+  
+  url_by_tag = requests.get(base_url + link)
+  data = url_by_tag.text
+  
+  lists = BeautifulSoup(data)
+  li = lists.find_all("li", attrs={"class": "dth-articles__item"})
+
+  for li2 in li:
+  
+    links_and_sections[key].append((li2.a["href"], li2.a.text))
+
+
+print(links_and_sections)
